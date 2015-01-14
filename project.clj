b9b9c494e12fe6e24f26f138a5496e915e6bbd41
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-01-14 11:36:12 +0100

    Setting up the project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject io.s6/psl "0.1.0-SNAPSHOT"
+  :description "Public Suffix List implementation"
+  :url "http://screen6.io/opensource/clojure/psl"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :scm :git
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.1"]])
